5bd341f94375096fc5677f6c4670c886e346e5cd
AT1903 <99317008+AT1903@users.noreply.github.com>
2022-02-15 12:50:21 +0500

    print_dict

---
+++ print_dict.py
@@ -0,0 +1,15 @@
+import pprint
+d = {'key1':{'k11':['v11','v112'],'k12': 'v12','k13':'v13'},'key2':{'k21':'v21','k22': 'v22','k23':'v23'}}
+print(d)
+
+
+def print_d(d, indent=0):
+    s = "...."
+    for key, value in d.items():
+        print(s * indent + str(key))
+        if isinstance(value, dict): # вернет True, если проверяемый объект object является экземпляром словаря
+            print_d(value, indent+1)
+        else:
+            print(s * (indent+1) + str(value))
+
+print_d(d)
